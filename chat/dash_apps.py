3b560914f260b74d31556a28a7b0b6a176cb699f
seanjust <seannjust@gmail.com>
2021-03-16 01:22:15 +0100

    Updating now and added a dropdown selector for Day Hour and 4 hour range.

--- chat/dash_apps.py
+++ chat/dash_apps.py
@@ -9,41 +9,45 @@ from django_plotly_dash import DjangoDash
 
 app = DjangoDash('SimpleExample')
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-df = pd.DataFrame(list(Cigar.objects.order_by('pub_date').values()))
-filt = df.loc[df['stopped'] == 1]
-df = pd.DataFrame(list(Cigar.objects.order_by('pub_date').values()))
-nofilt = df.loc[df['stopped'] == -1]
+
 #df = px.data.tips()
+def figuron():
+    df = pd.DataFrame(list(Cigar.objects.order_by('pub_date').values()))
+    filt = df.loc[df['stopped'] == 1]
+    df = pd.DataFrame(list(Cigar.objects.order_by('pub_date').values()))
+    nofilt = df.loc[df['stopped'] == -1]
+    print("Pues llamo a figure")
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(
+        x=filt['pub_date'],
+        y=filt['stopped'],
+        name='control', # name used in legend and hover labels
+        xbins=dict( # bins used for histogram
+            end='2021-3-31 00:00',
+            size= 3600000.0,
+            start='2021-1-01 00:00'
+        ),
+        marker_color='green',
+        opacity=0.75
+    ))
 
-fig = go.Figure()
-fig.add_trace(go.Histogram(
-    x=filt['pub_date'],
-    y=filt['stopped'],
-    name='control', # name used in legend and hover labels
-    xbins=dict( # bins used for histogram
-        end='2021-3-31 12:00',
-        size= 3600000.0,
-        start='2021-1-01 12:00'
-    ),
-    marker_color='green',
-    opacity=0.75
-))
-
-fig.add_trace(go.Histogram(
-    x=nofilt['pub_date'],
-    y=nofilt['stopped'],
-    name='control', # name used in legend and hover labels
-    xbins=dict( # bins used for histogram
-        end='2021-3-31 12:00',
-        size= 3600000.0,
-        start='2021-1-01 12:00'
-    ),
-    marker_color='red',
-    opacity=0.75
-))
-fig.update_layout(barmode='stack')
+    fig.add_trace(go.Histogram(
+        x=nofilt['pub_date'],
+        y=nofilt['stopped'],
+        name='control', # name used in legend and hover labels
+        xbins=dict( # bins used for histogram
+            end='2021-3-31 00:00',
+            size= 3600000.0,
+            start='2021-1-01 00:00'
+        ),
+        marker_color='red',
+        opacity=0.75
+    ))
+    fig.update_layout(barmode='stack')
+    return fig
 
 def layouto():
+    print("pues llamamos a layout")
     return html.Div(children=[
     html.H1(children='Hello Dash'),
 
@@ -54,9 +58,16 @@ def layouto():
 
 
     dcc.Graph(
-        id='example-graph',
+        id='egraph',
 
-        figure= fig
+        figure= figuron()
+    ),
+    dcc.Dropdown(
+        id='date-pick',
+        options=[{'label': 'Day', 'value': 3600000*24},
+                 {'label': 'Hour', 'value': 3600000},
+                 {'label': '4 Hour', 'value': 3600000*4}],
+        value=3600000
     ),
     html.Button('Submit', id='submit-val', n_clicks=0),
     html.Div([
@@ -68,10 +79,47 @@ app.layout = layouto
 
 
 @app.callback(dash.dependencies.Output('hoverdata-text','children'),
-             [dash.dependencies.Input('example-graph','figure')])
+             [dash.dependencies.Input('egraph','figure')])
 def callback_stats(hoverData):
     return str(hoverData['data'][0]['xbins'])
 
+
+@app.callback(dash.dependencies.Output('egraph', 'figure'),
+             [dash.dependencies.Input('date-pick', 'value')])
+def ojala(child):
+    df = pd.DataFrame(list(Cigar.objects.order_by('pub_date').values()))
+    filt = df.loc[df['stopped'] == 1]
+    df = pd.DataFrame(list(Cigar.objects.order_by('pub_date').values()))
+    nofilt = df.loc[df['stopped'] == -1]
+    print("Pues llamo a figure")
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(
+        x=filt['pub_date'],
+        y=filt['stopped'],
+        name='control',  # name used in legend and hover labels
+        xbins=dict(  # bins used for histogram
+            end='2021-3-31 00:00',
+            size=child,
+            start='2021-1-01 00:00'
+        ),
+        marker_color='green',
+        opacity=0.75
+    ))
+
+    fig.add_trace(go.Histogram(
+        x=nofilt['pub_date'],
+        y=nofilt['stopped'],
+        name='control',  # name used in legend and hover labels
+        xbins=dict(  # bins used for histogram
+            end='2021-3-31 00:00',
+            size=child,
+            start='2021-1-01 00:00'
+        ),
+        marker_color='red',
+        opacity=0.75
+    ))
+    fig.update_layout(barmode='stack')
+    return fig
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
