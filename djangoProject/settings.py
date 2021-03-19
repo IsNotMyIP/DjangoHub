f67ea381070050ba158bc4f542c9abc0fbdd309c
seanjust <seannjust@gmail.com>
2021-03-19 20:33:58 +0100

    Seems it works

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -120,6 +120,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+#Login things?
+LOGIN_URL = '/login/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
