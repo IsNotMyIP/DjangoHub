873e3a70af17cda4ecbc47f5dfeeb862e7b89e0a
seanjust <seannjust@gmail.com>
2021-03-19 16:05:05 +0100

    Changed DB to Postgre¿

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -79,13 +79,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
-}
+
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
