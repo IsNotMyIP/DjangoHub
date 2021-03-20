b885831b83cff1db1538da9af8dad6e8324d4718
SeanJust <seannjust@gmail.com>
2021-03-20 12:14:49 +0100

    Added Dashboard back

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'chat'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dashboard/', views.dashboard, name='test'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('smoke/', views.toSmoke, name='smoke'),
     path('cigar/', views.cigars, name='cigars'),
     path('login/', views.login, name='chat_login'),
